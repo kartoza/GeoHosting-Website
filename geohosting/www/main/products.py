500c1b522e4d1773b417fa541cdd7f03afcdd609
tinashechiraya <tinashe@kartoza.com>
2024-04-24 09:28:03 +0200

    testing context

--- geohosting/www/main/products.py
+++ geohosting/www/main/products.py
@@ -4,5 +4,6 @@ def get_context(context):
 
     products = frappe.db.sql("""SELECT * FROM `tabProducts`;""", as_dict=True)
     context.products = products
+    context.text = "this is a test"
 
     return context
