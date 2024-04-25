6ad637a2fabe2ac7902f2b75ea0bc7b86c14786f
tinashechiraya <tinashe@kartoza.com>
2024-04-25 12:54:27 +0200

    test fetching stock items

--- geohosting/www/main/products.py
+++ geohosting/www/main/products.py
@@ -1,11 +1,23 @@
 import frappe
 
-def get_context(context):
+# def get_context(context):
 
-    products = frappe.db.sql("""SELECT product_name, product_description, product_price, product_image FROM `tabProducts`;""", as_dict=True)
-    context.products = products
+#     products = frappe.db.sql("""SELECT product_name, product_description, product_price, product_image FROM `tabProducts`;""", as_dict=True)
+#     context.products = products
+
+#     return context
+
+def get_context(context):
+    # Fetch stock items from ERPNext database
+    stock_items = frappe.get_all("Item", 
+                                 filters={"is_stock_item": 1, "disabled": 0, "item_group": "GeoHosting"}, 
+                                 fields=["name", "item_name", "item_group", "stock_uom", "description", "disabled"])
+
+    # Add filtered stock items to the context
+    context.stock_items = stock_items
 
     return context
+
 
 
 
