c2d11a088d88d628c54690b8234191fdebe3c4c9
tinashechiraya <tinashe@kartoza.com>
2024-04-25 11:33:36 +0200

    add update logo and fonts

--- geohosting/www/main/products.py
+++ geohosting/www/main/products.py
@@ -2,8 +2,23 @@ import frappe
 
 def get_context(context):
 
-    products = frappe.db.sql("""SELECT * FROM `tabProducts`;""", as_dict=True)
+    products = frappe.db.sql("""SELECT product_name, product_description, product_price, product_image FROM `tabProducts`;""", as_dict=True)
     context.products = products
-    context.text = "this is a test"
 
     return context
+
+
+
+@frappe.whitelist(allow_guest=True)
+def get_product_details(product_id=None):
+    # Fetch product details based on the provided product ID
+    product = frappe.get_doc("Products", product_id)
+
+    if product:
+        return {
+            "name": product.item_name,
+            "description": product.description,
+            "image": product.image
+        }
+    else:
+        return {"error": "Product not found"}
