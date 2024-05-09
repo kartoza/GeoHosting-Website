c0d1983a0af626aa55250fa47aaac45a4997919c
tinashechiraya <tinashe@kartoza.com>
2024-05-09 21:17:41 +0200

    implement support tickets

---
+++ geohosting/www/portal/support_tickets.py
@@ -0,0 +1,27 @@
+import frappe
+
+from datetime import datetime, date, timedelta
+
+def serialize_datetime(obj):
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    elif isinstance(obj, timedelta):
+        # Convert timedelta to seconds for serialization
+        return obj.total_seconds()
+    return obj
+
+def get_context(context):
+
+    support_tickets = frappe.db.get_all("Issue", filters={
+        'owner': ['=', frappe.session.user],
+    }, fields='*', order_by="name") 
+
+    for support_ticket in support_tickets:
+        for key, value in support_ticket.items():
+            if isinstance(value, (datetime, date, timedelta)):
+                support_ticket[key] = serialize_datetime(value)
+
+
+    context.support_tickets = support_tickets
+
+    return context
