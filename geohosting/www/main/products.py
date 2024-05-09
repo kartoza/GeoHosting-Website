8f2c56e1bc11124211de574bfda82774f976008d
tinashechiraya <tinashe@kartoza.com>
2024-05-09 18:33:40 +0200

    implement invoices and other general improvements

--- geohosting/www/main/products.py
+++ geohosting/www/main/products.py
@@ -2,17 +2,13 @@ import frappe
 
 
 def get_context(context):
-    # Fetch stock items from database
     stock_items = frappe.get_all("Item", 
                                  filters={"is_stock_item": 1, 
                                           "disabled": 0, 
                                           "item_group": "GeoHosting",
-                                          "variant_of": ""},  # Filter out variants
+                                          "variant_of": ""},
                                  fields=["name", "item_name", "item_group", "stock_uom", "description", "disabled", "image"])
 
-    # Add filtered stock items to the context
     context.stock_items = stock_items
 
     return context
-
-
