8bbf325607f5765edaedbedb967f5ff688d9d8cf
tinashechiraya <tinashe@kartoza.com>
2024-06-24 13:16:05 +0200

    updates to site

--- geohosting/www/main/products.py
+++ geohosting/www/main/products.py
@@ -4,10 +4,12 @@ no_cache = 1
 
 def get_context(context):
     stock_items = frappe.get_all("Item", 
-                                 filters={"is_stock_item": 1, 
+                                 filters={
+                                        # "is_stock_item": 1, 
                                           "disabled": 0, 
                                           "item_group": "GeoHosting",
-                                          "variant_of": ""},
+                                          "variant_of": "",
+                                          "has_variants": 1},
                                  fields=["name", "item_name", "item_group", "stock_uom", "description", "disabled", "image"])
 
     context.stock_items = stock_items
